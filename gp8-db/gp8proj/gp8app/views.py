df1d7eb63868568547fc4e5daaa7fbaee9a9a666
Qinhao Zhang <lbjason0802@gmail.com>
2024-03-01 00:16:46 -0800

    url created

--- gp8-db/gp8proj/gp8app/views.py
+++ gp8-db/gp8proj/gp8app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import User
+
+
+def testmysql(request):
+    user = User.objects.all()
+
+
+context = {
+    'user_id': user[0].userid,
+    'user_gender': user[3].gender,
+}
+return render(request, 'home.html', context)
+
 
 # Create your views here.
