b3d8518b5f0d84b33d364d3062d8492558081c8e
AnningTian <tian.ann@northeastern.edu>
2024-03-01 18:28:34 -0800

    Updated CSS, JS, HTML, Settings

--- gp8-db/gp8proj/gp8app/views.py
+++ gp8-db/gp8proj/gp8app/views.py
@@ -4,14 +4,31 @@ from .models import User
 
 
 def testmysql(request):
-    user = User.objects.all()
+    search_query = request.GET.get('search_query')  # Retrieve the search term from the GET request
 
+    if search_query:
+        users = User.objects.filter(userid__icontains=search_query)
+    else:
+        users = User.objects.all()
 
-    context = {
-    'user_id': user[0].userid,
-    'user_gender': user[0].gender,
-    }
+    if users:
+        context = {
+            'user_id': users[0].userid,
+            'user_gender': users[0].gender,
+            'user_age': users[0].age,
+            'user_phone': users[0].phone,
+            'user_email': users[0].email,
+
+        }
+    else:
+        context = {
+            'user_id': 'Not found',
+            'user_gender': 'Not found',
+            'user_age': 'Not found',
+            'user_phone': 'Not found',
+            'user_email': 'Not found',
+
+        }
+        
     return render(request, 'home.html', context)
-
-
 
